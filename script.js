0361ba8d41e536c4f0ce2b8f6c2c18ee499cd634
Ethan Wylie <ethanwylie101@gmail.com>
2025-03-07 20:47:12 -0800

    Decided Website purpose

---
+++ script.js
@@ -0,0 +1,19 @@
+function popup(){
+    
+    alert("Sent Information!");
+
+
+
+
+    return 0;
+}
+
+function information(){
+
+    let xbox1 = document.getElementById("info-6");
+
+    onclick(alert(xbox1));
+
+
+    return 0;
+}
