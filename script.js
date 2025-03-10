763709bd441a974ce52bc555eb31f05957e72d62
Ethan Wylie <ethanwylie101@gmail.com>
2025-03-10 13:26:58 -0700

    added new page

--- script.js
+++ script.js
@@ -2,10 +2,7 @@ function popup(){
     
     alert("Sent Information!");
 
-
-
-
-    return 0;
+     return 0;
 }
 
 function information(){
@@ -13,6 +10,12 @@ function information(){
     let xbox1 = document.getElementById("info-6");
 
     onclick(alert(xbox1));
+
+
+    return 0;
+}
+
+function button(){
 
 
     return 0;
